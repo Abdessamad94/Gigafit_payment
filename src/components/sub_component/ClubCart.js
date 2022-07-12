f4c0d540e66a4e6d587a87ddc9fb44261ea6b9d8
Abdessamad94 <elbroud94@gmail.com>
2022-07-12 15:50:09 +0200

    club cart(Tuesday, July 12, 202)

--- src/components/sub_component/ClubCart.js
+++ src/components/sub_component/ClubCart.js
@@ -15,12 +15,12 @@ function ClubCart({ id, name, addres, idclub, setIdclub, setPage, page }) {
       <div className="set">
         <div className="cart-body">
           <span className="club-id">{id}</span>
-          <span className="club-name">{name}</span>
           <span className="club-addres">{addres}</span>
+          <span className="club-name">{name}</span>
           <div className="club-info">
-            <span><FontAwesomeIcon icon={faFacebook} /></span>
-            <span><FontAwesomeIcon icon={faInstagram} /></span>
-            <span><FontAwesomeIcon icon={faEnvelope} /></span>
+            <a className="fb" href="#"><span><FontAwesomeIcon icon={faFacebook} /></span></a>
+            <a className="inst" href="#"><span><FontAwesomeIcon icon={faInstagram} /></span></a>
+            <a className="mail" href="#"><span><FontAwesomeIcon icon={faEnvelope} /></span></a>
           </div>
         </div>
         <div className="footer">
