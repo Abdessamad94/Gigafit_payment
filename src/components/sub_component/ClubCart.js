5abd87e89046d4b8629a734382521bc80d4dcd37
Abdessamad94 <elbroud94@gmail.com>
2022-07-28 17:41:41 +0200

    club (Thursday, July 28, 2022) v2

--- src/components/sub_component/ClubCart.js
+++ src/components/sub_component/ClubCart.js
@@ -14,13 +14,15 @@ function ClubCart({ id, idclub, info , setIdclub , setPage, page }) {
       </div>
       <div className="set">
         <div className="cart-body">
-          <span className="club-id">{idclub}</span>
+          <span className="club-id" style={{display:"none"}}>{idclub}</span>
           <span className="club-name">{info.name.replace('Gigafit ', '')}</span>
           <span className="club-addres">{info.adresse}</span>
+          <a className="tel" href={`tel:${info.tel}`} target="_blanck"><span className="club-tel">{info.tel}</span></a>
           <div className="club-info">
             <a className="fb" href={info.facebook} target="_blanck"><span><FontAwesomeIcon icon={faFacebook}/></span></a>
             <a className="inst" href={info.instagram} target="_blanck"><span><FontAwesomeIcon icon={faInstagram} /></span></a>
             <a className="mail" href={`mailto:${info.email}`} target="_blanck"><span><FontAwesomeIcon icon={faEnvelope} /></span></a>
+            <a className="mail" href={info.siteWeb} target="_blanck"><span><FontAwesomeIcon icon={faEnvelope} /></span></a>
           </div>
         </div>
         <div className="footer">
